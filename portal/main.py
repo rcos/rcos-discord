b7207d57502d8bb4f62092505a041bf6145706e4
Frank Matranga <thefrankmatranga@gmail.com>
2021-01-22 16:18:32 -0500

    Update for Postgrest

--- portal/main.py
+++ portal/main.py
@@ -3,18 +3,19 @@ import os
 import pytz
 import requests
 from dotenv import load_dotenv
-from flask import (Flask, g, flash, redirect, render_template, request, session,
-                   url_for, abort)
+from flask import (Flask, abort, flash, g, redirect, render_template, request,
+                   session, url_for)
 from flask_cas import CAS, login_required, logout
 from flask_talisman import Talisman
 from werkzeug.exceptions import HTTPException
 
 from .discord import (OAUTH_URL, SERVER_ID, VERIFIED_ROLE_ID,
-                     add_role_to_member, add_user_to_server, get_member,
-                     get_tokens, get_user_info, kick_member_from_server,
-                     set_member_nickname)
+                      add_role_to_member, add_user_to_server, get_member,
+                      get_tokens, get_user_info, kick_member_from_server,
+                      set_member_nickname)
 from .rcos import (delete_user_discord_account, fetch_user,
-                  fetch_user_discord_account, upsert_user, upsert_user_discord_account)
+                   fetch_user_discord_account, create_or_update_user,
+                   create_or_update_user_discord_account)
 
 # Load .env into os.environ
 load_dotenv()
@@ -40,6 +41,9 @@ app.config['SITE_TITLE'] = os.environ.get('SITE_TITLE')
 app.config['CAS_SERVER'] = 'https://cas-auth.rpi.edu/cas'
 app.config['CAS_AFTER_LOGIN'] = 'join'
 
+DISCORD_SERVER_INVITE_URL = os.environ.get('DISCORD_SERVER_INVITE_URL')
+
+
 @app.before_request
 def before_request():
     '''Runs before every request.'''
@@ -47,59 +51,63 @@ def before_request():
     # Try to fetch user
     if 'user' not in session and cas.username:
         session['user'] = fetch_user(cas.username.lower())
-
     # Try to fetch user's discord account
     if ('user_discord_account' not in session or session['user_discord_account'] is None) and cas.username:
-        session['user_discord_account'] = fetch_user_discord_account(cas.username.lower())
+        session['user_discord_account'] = fetch_user_discord_account(
+            cas.username.lower())
+
+    g.is_logged_in = cas.username is not None
+    g.username = cas.username.lower() if g.is_logged_in else None
+    g.user = session['user'] if 'user' in session else None
+    g.identifier = f'RPI user {g.username}' if g.is_logged_in else 'external user '
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if g.is_logged_in:
+        return redirect(url_for('join'))
+    return render_template('index.html', discord_server_invite_url=DISCORD_SERVER_INVITE_URL)
 
 
 @app.route('/join', methods=['GET', 'POST'])
+@login_required
 def join():
-    is_logged_in = cas.username is not None
-    username = cas.username.lower() if is_logged_in else None
-    user = session['user'] if is_logged_in else None
-    
     if request.method == 'GET':
-        if is_logged_in:
-            app.logger.info(f'Home page requested by RPI user {cas.username}')
-        else:
-            app.logger.info(f'Home page requested by external user')
-
         # Already connected to Discord!
-        if session['user_discord_account']:
+        if 'user_discord_account' in session and session['user_discord_account']:
             return redirect(url_for('joined'))
 
-        return render_template('join.html', is_logged_in=is_logged_in, user=user, username=username, timezones=pytz.all_timezones)
+        return render_template('join.html', is_logged_in=True, is_student=True, username=g.username, user=g.user, timezones=pytz.all_timezones)
     elif request.method == 'POST':
         # Limit to 20 characters so overall Discord nickname doesn't exceed limit of 32 characters
         first_name = request.form['first_name'].strip()[:20]
         last_name = request.form['last_name'].strip()
-        graduation_year = int(request.form['graduation_year'].strip())
 
         if len(first_name) == 0 or len(last_name) == 0:
             flash('Nice try... Please enter a name.', category='error')
             return redirect(url_for('index'))
 
-        if graduation_year > 2038 or graduation_year < 2000:
-            flash('Nice try... Stick to the allowed graduation year range.', category='error')
-            return redirect(url_for('index'))
-
-
         user = {
+            'username': g.username,
             'first_name': first_name,
             'last_name': last_name,
-            'graduation_year': graduation_year,
+            'role': 'student',
             'timezone': request.form['timezone']
         }
-        
-        # This will either create or update the user
-        session['user'] = upsert_user(cas.username.lower(), user)
 
-        app.logger.info(f'Redirecting {cas.username} to Discord OAuth page')
+        if 'graduation_year' in request.form and len(request.form['graduation_year']):
+            graduation_year = int(request.form['graduation_year'].strip())
+            if graduation_year > 2038 or graduation_year < 2000:
+                flash(
+                    'Nice try... Stick to the allowed graduation year range.', category='error')
+                return redirect(url_for('index'))
+
+            user['cohort'] = graduation_year - 4
+
+            # This will ensure the user now exists
+            session['user'] = create_or_update_user(g.username, user)
+
+        app.logger.info(f'Redirecting {g.identifier} to Discord OAuth page')
         return redirect(OAUTH_URL)
 
 
@@ -114,23 +122,25 @@ def discord_callback():
         # Handle the special case where the user declined to connect
         if error == 'access_denied':
             app.logger.error(
-                f'{cas.username} declined to connect their Discord account')
+                f'{g.identifier} declined to connect their Discord account')
             return render_template('error.html', error='You declined to connect your Discord account!')
         else:
             # Handle generic Discord error
             error_description = request.args.get('error_description')
             app.logger.error(
-                f'An error occurred on the Discord callback for {cas.username}: {error_description}')
+                f'An error occurred on the Discord callback for {g.identifier}: {error_description}')
             raise Exception(error_description)
 
     # Get user from DB
     user = session['user']
 
-    # Generate nickname as "<first name> <last name initial> '<2 digit graduation year> (<rcs id>)"
-    # e.g. "Frank M '22 (matraf)"
+    # Generate nickname as "<first name> <last name initial> '<2 digit graduation year>"
+    # e.g. "Frank M '22"
     nickname = user['first_name'] + ' ' + \
-        user['last_name'][0] + " '" + str(user['graduation_year'])[2:] + \
-        f' ({cas.username.lower()})'
+        user['last_name'][0]
+
+    if user['cohort']:
+        nickname += " '" + str(user['cohort'] + 4)[2:]
 
     # Exchange authorization code for tokens
     tokens = get_tokens(authorization_code)
@@ -139,29 +149,29 @@ def discord_callback():
     discord_user = get_user_info(tokens['access_token'])
 
     # Save to DB
-    upsert_user_discord_account(user['username'], discord_user['id'])
+    create_or_update_user_discord_account(user['username'], discord_user['id'])
 
     # Add them to the server
     add_user_to_server(tokens['access_token'],
-                        discord_user['id'], nickname)
-    app.logger.info(f'Added {cas.username} to Discord server')
+                       discord_user['id'], nickname)
+    app.logger.info(f'Added {g.identifier} to Discord server')
 
     # Set their nickname
     try:
         set_member_nickname(discord_user['id'], nickname)
         app.logger.info(
-            f'Set {cas.username}\'s nickname to "{nickname}" on server')
+            f'Set {g.username}\'s nickname to "{nickname}" on server')
     except requests.exceptions.HTTPError as e:
         app.logger.warning(
-            f'Failed to set nickname "{nickname}" to {cas.username} on server: {e}')
+            f'Failed to set nickname "{nickname}" to {g.username} on server: {e}')
 
     # Give them the verified role
     try:
         add_role_to_member(discord_user['id'], VERIFIED_ROLE_ID)
-        app.logger.info(f'Added verified role to {cas.username} on server')
+        app.logger.info(f'Added verified role to {g.username} on server')
     except requests.exceptions.HTTPError as e:
         app.logger.warning(
-            f'Failed to add role to {cas.username} on server: {e}')
+            f'Failed to add role to {g.username} on server: {e}')
 
     return redirect(url_for('joined'))
 
--- portal/rcos.py
+++ portal/rcos.py
@@ -1,57 +1,101 @@
 from typing import Dict, Optional
 import requests
 import os
+import jwt
 
 from requests.exceptions import HTTPError
 
-api_base_url = 'https://rcos-api.herokuapp.com/api/v1'
+API_URL = os.environ.get('RCOS_API_URL')
+JWT_SECRET = os.environ["POSTGREST_JWT_SECRET"]
 
-headers = {
-    'api_key': os.environ['RCOS_API_KEY']
-}
 
-def upsert_user(username: str, user: Dict) -> Optional[Dict]:
-    r = requests.put(f'{api_base_url}/users/{username}', json={
-        **user,
-        'is_rpi': True,
-        'is_faculty': False
-    }, headers=headers)
+# Create JSON Web Token to authenticate Postgrest
+encoded_jwt = jwt.encode({"role": "api_user"}, JWT_SECRET,
+                         algorithm="HS256")
+
+
+api = requests.Session()
+api.headers['Authorization'] = 'Bearer ' + encoded_jwt
+
+
+def create_or_update_user(username: str, user: Dict) -> Optional[Dict]:
+    r = api.put(f'{API_URL}/users', params={
+        'username': 'eq.' + username
+    }, json=user, headers={
+        'Prefer': 'return=representation'
+    })
     try:
         r.raise_for_status()
     except HTTPError as err:
         print(err)
+        print(err.response.json())
         return None
-    return r.json()
+    return r.json()[0]
+
 
 def fetch_user(username: str) -> Optional[Dict]:
-    r = requests.get(api_base_url + '/users/' + username, headers=headers)
+    r = api.get(API_URL + '/users', params={
+        'username': 'eq.' + username
+    }, headers={
+        'Accept': 'application/vnd.pgrst.object+json'
+    })
     try:
         r.raise_for_status()
     except HTTPError as err:
         print(err)
+        print(err.response.json())
         return None
     return r.json()
+
 
 def fetch_user_discord_account(username: str) -> Optional[Dict]:
-    r = requests.get(f'{api_base_url}/users/{username}/accounts/discord', headers=headers)
+    r = api.get(f'{API_URL}/user_accounts', params={
+        'username': 'eq.' + username,
+        'type': 'eq.discord'
+    }, headers={
+        'Authentication': 'Bearer ' + encoded_jwt,
+        'Accept': 'application/vnd.pgrst.object+json'
+    })
     try:
         r.raise_for_status()
     except HTTPError as err:
+        print(err)
+        print(err.response.json())
         return None
     return r.json()
 
-def upsert_user_discord_account(username: str, discord_user_id: str):
-    r = requests.put(f'{api_base_url}/users/{username}/accounts/discord', json={ 'account_id': discord_user_id }, headers=headers)
+
+def create_or_update_user_discord_account(username: str, discord_user_id: str):
+    r = api.put(f'{API_URL}/user_accounts', params={
+        'username': 'eq.' + username,
+        'type': 'eq.discord'
+    }, json={
+        'username': username,
+        'type': 'discord',
+        'account_id': discord_user_id
+    }, headers={
+        'Prefer': 'return=representation'
+    })
     try:
         r.raise_for_status()
     except HTTPError as err:
+        print(err)
+        print(err.response.json())
         return None
     return r.json()
+
 
 def delete_user_discord_account(username: str):
-    r = requests.delete(f'{api_base_url}/users/{username}/accounts/discord', headers=headers)
+    r = api.delete(f'{API_URL}/user_accounts', params={
+        'username': 'eq.' + username,
+        'type': 'eq.discord'
+    }, headers={
+        'Prefer': 'return=representation'
+    })
     try:
         r.raise_for_status()
     except HTTPError as err:
+        print(err)
+        print(err.response.json())
         return None
     return r.json()
